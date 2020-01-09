71bbe9e62de10b4953db69762c8dc6449e468b64
haowu <hao.wu@campus.tu-berlin.de>
2020-01-09 23:14:28 +0100

    Goat:fertig; neue Class:Rock erstellen

---
+++ src/hausaufgabe/Rock.java
@@ -0,0 +1,14 @@
+package hausaufgabe;
+
+import gridworld.framework.actor.Actor;
+
+import java.awt.*;
+
+public class Rock extends Actor {
+
+    public Rock()
+    {
+        this.setColor(Color.RED);
+    }
+
+}
