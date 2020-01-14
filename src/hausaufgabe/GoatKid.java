e260d8f4fa2d544e56321cc471d7e7ef31195c5d
stepperstep <stefan.schulz.94@gmail.com>
2020-01-14 17:02:14 +0100

    - MilkStorage bearbeitet - MilkStorageDisplay erstellt. - Goatkid wird mit age=3 zu Goat. - Goat erbt von GoatKid - Goat hat nun GoatMilk - Goat hat nun 2 Konstruktoren. - Goat wird nun auch zur Blume. - Goatkid hat noch zu viele Berechtigungen :( - viele Kommentare in verschiedenen Klassen.
    PROBLEM FarmWorldRunner

--- src/hausaufgabe/GoatKid.java
+++ src/hausaufgabe/GoatKid.java
@@ -48,27 +48,9 @@ public class GoatKid extends Animal {
             Location nextNORTH = loc.getAdjacentLocation(Location.NORTH);
             Location nextNORTHEAST = loc.getAdjacentLocation(Location.NORTHEAST);
 
-/*        List<Location> surroundingLocation = new ArrayList<>();
 
-        surroundingLocation.add(nextEAST);
-        surroundingLocation.add(nextSOUTHEAST);
-        surroundingLocation.add(nextSOUTH);
-        surroundingLocation.add(nextSOUTHWEST);
-        surroundingLocation.add(nextWEST);
-        surroundingLocation.add(nextNORTHWEST);
-        surroundingLocation.add(nextNORTH);
-        surroundingLocation.add(nextNORTHEAST);
-
-        for (Location location : surroundingLocation) {
-            if (gr.isValid(location))
-                moveTo(location);
-            else
-                removeSelfFromGrid();
-                countGoatNumber = countGoatNumber - 1;
-//            Flower flower = new Flower(getColor());
-//            flower.putSelfInGrid(gr, loc);
-        }*/
-
+// --------> hier könnte ein switch sehr hilfreich sein!! <---------
+// ------> muss für GOAT noch anpassen <---------------
             if (gr.isValid(nextEAST)) {
                 GoatKid kid = new GoatKid();
                 kid.putSelfInGrid(getGrid(), nextEAST);
@@ -176,93 +158,6 @@ public class GoatKid extends Animal {
             moveOnTheRock();
         }
 
-
-/*        Location next1steps = loc.getAdjacentLocation(changedDirection);
-        Location next2steps = next1steps.getAdjacentLocation(changedDirection);
-        Location next3steps = next2steps.getAdjacentLocation(changedDirection);
-        Location next4steps = next3steps.getAdjacentLocation(changedDirection);
-        Location next5steps = next4steps.getAdjacentLocation(changedDirection);
-        Location next6steps = next5steps.getAdjacentLocation(changedDirection);
-        Location next7steps = next6steps.getAdjacentLocation(changedDirection);
-        Location next8steps = next7steps.getAdjacentLocation(changedDirection);
-        Location next9steps = next8steps.getAdjacentLocation(changedDirection);
-        Location next10steps = next9steps.getAdjacentLocation(changedDirection);
-        Location next11steps = next10steps.getAdjacentLocation(changedDirection);
-        Location next12steps = next11steps.getAdjacentLocation(changedDirection);
-        Location next13steps = next12steps.getAdjacentLocation(changedDirection);
-        Location next14steps = next13steps.getAdjacentLocation(changedDirection);
-        Location next15steps = next14steps.getAdjacentLocation(changedDirection);
-        Location next16steps = next15steps.getAdjacentLocation(changedDirection);
-        Location next17steps = next16steps.getAdjacentLocation(changedDirection);
-        Location next18steps = next17steps.getAdjacentLocation(changedDirection);
-        Location next19steps = next18steps.getAdjacentLocation(changedDirection);
-
-        Actor neighbor1steps = gr.get(next1steps);
-        Actor neighbor2steps = gr.get(next2steps);
-        Actor neighbor3steps = gr.get(next3steps);
-        Actor neighbor4steps = gr.get(next4steps);
-        Actor neighbor5steps = gr.get(next5steps);
-        Actor neighbor6steps = gr.get(next6steps);
-        Actor neighbor7steps = gr.get(next7steps);
-        Actor neighbor8steps = gr.get(next8steps);
-        Actor neighbor9steps = gr.get(next9steps);
-        Actor neighbor10steps = gr.get(next10steps);
-        Actor neighbor11steps = gr.get(next11steps);
-        Actor neighbor12steps = gr.get(next12steps);
-        Actor neighbor13steps = gr.get(next13steps);
-        Actor neighbor14steps = gr.get(next14steps);
-        Actor neighbor15steps = gr.get(next15steps);
-        Actor neighbor16steps = gr.get(next16steps);
-        Actor neighbor17steps = gr.get(next17steps);
-        Actor neighbor18steps = gr.get(next18steps);
-        Actor neighbor19steps = gr.get(next19steps);
-
-
-        if (neighbor1steps instanceof Rock){
-            moveTo(next1steps) ;
-        } else if (neighbor2steps instanceof Rock) {
-            moveTo(next2steps) ;
-        } else if (neighbor3steps instanceof Rock) {
-            moveTo(next3steps) ;
-        } else if (neighbor4steps instanceof Rock) {
-            moveTo(next4steps) ;
-        } else if (neighbor5steps instanceof Rock) {
-            moveTo(next5steps) ;
-        } else if (neighbor6steps instanceof Rock) {
-            moveTo(next6steps) ;
-        } else if (neighbor7steps instanceof Rock) {
-            moveTo(next7steps) ;
-        } else if (neighbor8steps instanceof Rock) {
-            moveTo(next8steps) ;
-        } else if (neighbor9steps instanceof Rock) {
-            moveTo(next9steps) ;
-        } else if (neighbor10steps instanceof Rock) {
-            moveTo(next10steps) ;
-        } else if (neighbor11steps instanceof Rock) {
-            moveTo(next11steps) ;
-        } else if (neighbor12steps instanceof Rock) {
-            moveTo(next12steps) ;
-        } else if (neighbor13steps instanceof Rock) {
-            moveTo(next13steps) ;
-        } else if (neighbor14steps instanceof Rock) {
-            moveTo(next14steps) ;
-        } else if (neighbor15steps instanceof Rock) {
-            moveTo(next15steps) ;
-        } else if (neighbor16steps instanceof Rock) {
-            moveTo(next16steps) ;
-        } else if (neighbor17steps instanceof Rock) {
-            moveTo(next17steps) ;
-        } else if (neighbor18steps instanceof Rock) {
-            moveTo(next18steps) ;
-        } else if (neighbor19steps instanceof Rock) {
-            moveTo(next19steps) ;
-        }else{
-        }
-
-
-        if(canClimb ){
-            moveTo();
-        }*/
 
     }
 
