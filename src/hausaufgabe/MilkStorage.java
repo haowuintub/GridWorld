0ad877057372678a608247f197644e2b7546e68b
stepperstep <stefan.schulz.94@gmail.com>
2020-01-15 22:39:01 +0100

    - MilkStorage weiter bearbeitet, nicht fertig. - GoatMilker bearbeitet, noch nicht fertig. ---Body's: canMilkGoats & milkGoats fehlen! - viel simplifiziert, nicht jede simplifizierung ist gut!
    PROBLEM GoatKid Spawnpoint!
    PROBLEM MYOWNCRITTER !!!!!

---
+++ src/hausaufgabe/MilkStorage.java
@@ -0,0 +1,27 @@
+package hausaufgabe;
+
+import gridworld.framework.actor.Actor;
+
+public class MilkStorage extends Actor {
+
+//Attribute
+    public int milk;
+
+//Konstruktor
+    public MilkStorage(){
+        this.milk = 0;
+    }
+
+//Klassenmethoden
+    public void addMilk(){
+        this.milk =+ 1;
+    }
+
+    public void resetMilkStorage(){
+        this.milk = 0;
+    }
+
+    public String toString(){
+        return getClass().getName() + "milk=" + milk;
+    }
+}
